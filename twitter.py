7600818be177db73ca05afc3d16ac23fafdc3025
Patrick Rock <prock2011@gmail.com>
2016-03-26 14:37:45 -0500

    tweet multiple lines

--- twitter.py
+++ twitter.py
@@ -11,25 +11,25 @@ def get_api(cfg):
   auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
   return tweepy.API(auth)
 
+def tweet(text):
+  tweets = breakup.split_tweet(text)
+  for tweet in tweets:
+    api.update_status(status=tweet)
+
 def main():
   # Fill in the values noted in previous step here
   f = open('keys.txt','r')
   cfg_keys = ["consumer_key", "consumer_secret", "access_token" , "access_token_secret"]
 
-  for i = range(0,4):
+  for i in range(0,4):
       l = f.readline().strip()
       cfg[cfg_keys[i]] = l
 
   api = get_api(cfg)
   v = verse.get_verse()
-  while len(v[1]) >= 140:
-    v = verse.get_verse() 
-  tweet = v[1] + ' ' + translation.translate_to_converge(v[0],10)
-  print tweet
-  status = api.update_status(status=v[1]+v[0])
+  tweet(v[1]+v[0])
+  tweet(v[1] + ' ' + translation.translate_to_converge(v[0],10))
   status = api.update_status(status=tweet) 
-  # Yes, tweet is called 'status' rather confusing
-
 
 if __name__ == '__main__':
   main()
